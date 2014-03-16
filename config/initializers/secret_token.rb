d301392eee5d903b7005ef18e1f443abc4cb896f
Alyssa Diaz <alydiaz77@gmail.com>
2014-03-16 11:42:06 -0500

    initial commit for real

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shoelace::Application.config.secret_key_base = 'e284e294f4dcdcb9d6167bf0a4de725d034c549b05a7772e487d530f4b47f8783fde368a2c469470e2beeeb72dc5a1f4d810fe1ee6d5cd683bdf62dd376c2fdd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shoelace::Application.config.session_store :cookie_store, key: '_shoelace_session'
