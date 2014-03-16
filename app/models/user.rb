91cd25ec4075117525f710bca7fb513c0749c88e
thejessleigh <j.l.unrein@gmail.com>
2014-03-16 12:15:30 -0500

    Fixed wiring on database relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :appointments, foreign_key: mentor_id
+  has_many :appointments, foreign_key: :mentor_id
   has_many :mentees, through: :appointments
 
   has_many :sessions, class_name: "Appointment", foreign_key: :mentee_id
