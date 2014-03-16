243e9314302e3753fc53858d7e5d3ff9a1b2f4ef
Alyssa Diaz <alydiaz77@gmail.com>
2014-03-16 15:37:07 -0500

    Added Navbar Styling for login and Getting appointments to the Schedule Page

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,5 +1,6 @@
 class SchedulesController < ApplicationController
 
   def index
+    @appointments = Appointment.get_saturday_appointments
   end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,15 @@
 class Appointment < ActiveRecord::Base
   belongs_to :mentor, class_name: "User"
   belongs_to :mentee, class_name: "User"
+
+  def self.get_saturday_appointments
+    Appointment.where(:start_time => saturday.beginning_of_day..saturday.end_of_day)
+  end
+
+  private
+
+  def self.saturday
+    date = Time.now
+    date.wday == 6 ? date : date + (6 - date.wday).days
+  end
 end
