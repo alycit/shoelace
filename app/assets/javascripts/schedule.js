f0c875f2a90b1130172921a066b0a3363d0bf47e
Alyssa Diaz <alydiaz77@gmail.com>
2014-04-05 13:54:21 -0400

    Styling changes

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(".new_appt").on("click", function(event) {
+  $(".new_mentor_availibility").on("click", function(event) {
     $("#start_time").text($(this).text());
     $('#new_appointment').modal('show');
   });
