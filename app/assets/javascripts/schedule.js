10a65b97a7cffae93174bf2733dcb25d2949350d
Alyssa Diaz <alydiaz77@gmail.com>
2014-06-25 19:48:50 -0500

    removing alerts

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -4,14 +4,6 @@ $(document).ready(function(){
     $('#new_appointment').modal('show');
   });
 
-  $(".available").on("click", function(event){
-    alert("available");
-  });
-
-  $(".filled").on("click", function(event){
-    alert("filled");
-  });
-
   $("#create_appt").on("click", function(){
     $("input[name='start_time']").val($("#start_time").text());
     $("#create_appt_form").submit();
